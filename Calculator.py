c5b14a7dc23f719354e85d6108a5eb2eef69d9b4
mariammmramy <mariam.said06@eng-st.cu.edu.eg>
2025-09-14 22:53:35 +0300

    minor changes

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,22 @@
-import addition
-import Multiplication
+from addition import add_numbers
+from Multiplication import Multi
 def subtract(x, y):
     return x - y
 def main():
-    num1=input("Enter first number: ")
-    num2=input("Enter second number: ")
+    num1=int(input("Enter first number: "))
+    num2=int(input("Enter second number: "))
     op=input("Enter operation symbol")
 
     match(op):
         case "-":
             result=subtract(num1,num2)
         case "+":
-            result=addition(num1,num2)
-	case "*":
-            result=Multi(num1,num2)        
+            result=add_numbers(num1,num2)
+        case "*":
+            result=Multi(num1,num2)      
 
     print("Result is: {result}")
+
+if __name__ == "__main__":
+    main()
 
