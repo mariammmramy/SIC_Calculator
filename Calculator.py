c47c84d7c9a7867abfda607f2642532c60b71c04
mariammmramy <mariam.said06@eng-st.cu.edu.eg>
2025-09-14 22:25:52 +0300

    main function

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,15 @@
-#file
+
 def subtract(x, y):
     return x - y
+def main():
+    num1=input("Enter first number: ")
+    num2=input("Enter second number: ")
+    op=input("Enter operation symbol")
+
+    match(op):
+        case "-":
+            result=subtract(num1,num2)
+    
+
+    print("Result is: {result}")
+
